94ef022f386629d6b999d30ca4bdfb8ca416ef2e
adocai3 <adogravic@alu.edu.gva.es>
2025-04-14 21:37:13 +0200

    Update ejercicio2b.py

--- ejercicio2b.py
+++ ejercicio2b.py
@@ -1,9 +1,19 @@
+# coding=utf-8
+__Author__ = "Adonis Gramaje Victoria"
+
+# Función que determina si la persona es mayor de edad
+def esMayorEdad(edad):
+    return edad >= 18
+
+# Función principal
 def main():
     nombre = input("Introduzca su nombre: ")
     
     try:
         edad = int(input(f"Introduzca su edad, {nombre}: "))
-        if edad >= 18:
+        
+        # Usamos la función esMayorEdad para comprobar si puede conducir
+        if esMayorEdad(edad):
             print(f"{nombre}, ya puedes conducir.")
         else:
             print(f"{nombre}, todavía no puedes conducir.")
