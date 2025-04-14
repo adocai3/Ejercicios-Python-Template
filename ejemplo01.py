bcbec7c3493d8fbcf1278b2bae2a1fae0b57b576
adocai3 <adogravic@alu.edu.gva.es>
2025-04-14 20:28:20 +0200

    Update ejemplo01.py

--- ejemplo01.py
+++ ejemplo01.py
@@ -1,23 +1,26 @@
 # coding=utf-8
-__Author__="José Gaspar Sánchez García"
+__author__ = "Adonis Gramaje Victoria"
 
-import webbrowser, urllib.request, urllib.error, urllib.parse
+import webbrowser
 
 # Programa principal
 def main():
     print("MANEJO DE CADENAS")
-    print(30*"=")
-    nombre=input("Introduzca su nombre: ")
-    apellidos=input("Introduzca sus apellidos: ");
-    print("¡Hola {0} {1}!".format(nombre,apellidos))
-    print("MAYUSCULAS: {0}".format(nombre.upper()))
-    print("nombre en minúsculas: "+nombre.lower())
+    print("=" * 30)
+    
+    nombre = input("Introduzca su nombre: ")
+    apellidos = input("Introduzca sus apellidos: ")
+    
+    print("¡Hola {0} {1}!".format(nombre, apellidos))
+    print("MAYÚSCULAS: {0}".format(nombre.upper()))
+    print("nombre en minúsculas: " + nombre.lower())
 
     print("MANEJO DE PÁGINAS WEB")
-    print(30*"=")
-    url=input("Introduzca URL de la página web: ")
+    print("=" * 30)
+    
+    url = input("Introduzca URL de la página web: ")
     print("Abriendo página web {0} en una nueva pestaña.".format(url))
     webbrowser.open_new_tab(url)
-    
-if __name__== "__main__" :
-   main()
+
+if __name__ == "__main__":
+    main()
