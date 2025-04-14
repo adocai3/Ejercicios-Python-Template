2a78cc72f57a3d7442ec4ac09bcc06bf8e331261
adocai3 <adogravic@alu.edu.gva.es>
2025-04-14 21:23:56 +0200

    Update ejercicio7.py

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,25 +1,38 @@
 # coding=utf-8
 __Author__ = "Adonis Gramaje Victoria"
 
-# Función que genera la serie de Fibonacci hasta n elementos
-def fibonacci(n):
-    vector = []
+"""
+Escriba un programa que simule el juego Piedra, papel, tijera para dos jugadores.
+"""
 
-    if n < 1:
-        return vector
-    elif n == 1:
-        vector.append(1)
-        return vector
-    elif n >= 2:
-        vector.append(1)
-        vector.append(1)
-        for i in range(2, n):
-            siguiente = vector[i - 1] + vector[i - 2]
-            vector.append(siguiente)
+import random
 
-    return vector  # Retorno de la función
+# Función que determina quién gana
+# 0: Empate
+# 1: Gana Jugador 1
+# 2: Gana Jugador 2
+def quienGana(jugada1, jugada2):
+    if jugada1 == jugada2:
+        return 0
+    elif (jugada1 == "piedra" and jugada2 == "tijera") or \
+         (jugada1 == "tijera" and jugada2 == "papel") or \
+         (jugada1 == "papel" and jugada2 == "piedra"):
+        return 1
+    else:
+        return 2
 
 # Programa principal
 def main():
-    print("SERIE DE FIBONACCI")
-    numero = int(input("Introdu
+    print("PIEDRA, PAPEL, ... ¡TIJERA!")
+
+    nombre1 = input("Introduzca el nombre del Jugador 1: ")
+    nombre2 = input("Introduzca el nombre del Jugador 2: ")
+    numeroTirada = int(input("Introduzca el número de tiradas: "))
+
+    ganadas1 = 0
+    ganadas2 = 0
+
+    tirada = 1
+    while numeroTirada > 0:
+        print(f"\nTirada nº {tirada}:")
+        j1
