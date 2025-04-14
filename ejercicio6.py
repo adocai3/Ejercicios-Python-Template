f97067e2816be9dcd3acfefa83e9db9c87a8c64f
adocai3 <adogravic@alu.edu.gva.es>
2025-04-14 21:14:45 +0200

    Update ejercicio6.py

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,28 +1,29 @@
 # coding=utf-8
-__Author__="José Gaspar Sánchez García"
+__Author__ = "José Gaspar Sánchez García"
 
-
-# Función que determina si un numero es primo.
-
-def esPrimo(numero) :
+# Función que determina si un número es primo
+def esPrimo(numero):
     contador = 0
 
-    if numero==1 :
-        return True
+    if numero <= 1:
+        return False  # Por definición, 0 y 1 no son primos
 
-    # --> Implemente el código del Bucle <--
-    
-    # Si tiene solo dos divisores el número es primo     
-    if contador == 2 :
+    # Contar los divisores
+    for i in range(1, numero + 1):
+        if numero % i == 0:
+            contador += 1
+
+    # Si tiene solo dos divisores, el número es primo     
+    if contador == 2:
         return True
-    else :
+    else:
         return False
 
 # Programa principal
 def main():
     print("ES PRIMO")
-    n=int(input("Introduzca un numero: "))
-    print("{0} es primo --> {1}.".format(n,esPrimo(n)))
+    n = int(input("Introduzca un número: "))
+    print("{0} es primo --> {1}.".format(n, esPrimo(n)))
 
-if __name__== "__main__" :
-   main()
+if __name__ == "__main__":
+    main()
