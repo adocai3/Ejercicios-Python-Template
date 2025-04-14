1ffb819ef4e090a2376616594ac9dad5dde981fe
adocai3 <adogravic@alu.edu.gva.es>
2025-04-14 21:14:23 +0200

    Update ejercicio5.py

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,47 +1,21 @@
 # coding=utf-8
-__Author__="José Gaspar Sánchez García"
+__Author__ = "José Gaspar Sánchez García"
 
 import random
 
-# Función que determina si un numero es par.
+# Función que determina si un número es par
+def esPar(numero):
+    return numero % 2 == 0
 
-def esPar(numero) :
-    return True # --> Implemente código de la función <--
+# Función que determina si un número es impar
+def esImpar(numero):
+    return not esPar(numero)
 
-def esImpar(numero) :
-    return False # --> Implemente código de la función <--
-
-def generarPares(valores, inicio) :
-    pares=[]
-    numero=inicio
-    if esImpar(inicio) :
-        numero=inicio+1
-    # --> Complete código de la función <--
-
-    return pares
-
-def generarImpares(valores, inicio) :
-    impares=[]
-    numero=inicio
-    if esPar(inicio) :
-        numero=inicio+1
-
-    # --> Complete código de la función <--
-
-    return impares
-
-
-# Programa principal
-def main():
-    print("Par e impar")
-    n=int(input("Introduzca un numero: "))
-    print("{0} es par --> {1}.".format(n,esPar(n)))
-    m=int(input("Introduzca el número de valores: "))
-    i=int(input("Introduzca el número inicial: "))
-    x=generarPares(m,i)
-    y=generarImpares(m,i)
-    print("Impares: ",y)
-    print("Pares: ",x)    
-
-if __name__== "__main__" :
-   main()
+# Generar una lista de números pares
+def generarPares(valores, inicio):
+    pares = []
+    numero = inicio
+    if esImpar(numero):
+        numero += 1  # Asegurar que comienza en par
+    for _ in range(valores):
+        pares.append
