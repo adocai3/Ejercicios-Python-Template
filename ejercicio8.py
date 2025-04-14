f8a4fba1c3788341ef82c96e7be5a75ae63c9a0a
adocai3 <adogravic@alu.edu.gva.es>
2025-04-14 21:15:23 +0200

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,36 +1,25 @@
 # coding=utf-8
-__Author__="José Gaspar Sánchez García"
+__Author__ = "José Gaspar Sánchez García"
 
-
-# Función que determina si un numero es primo.
-
-def fibonacci(n) :
+# Función que genera la serie de Fibonacci hasta n elementos
+def fibonacci(n):
     vector = []
 
-    if n<1 :
+    if n < 1:
         return vector
-    elif n==1 :
+    elif n == 1:
         vector.append(1)
         return vector
-    elif n >=2 :
-        # Implementa las series de Fibonacci
-        # vector[0]=1
-        # vector[1]=1
+    elif n >= 2:
+        # Inicializamos los dos primeros valores
+        vector.append(1)
+        vector.append(1)
 
-        vector.append(1)
-        vector.append(1)
         # Creamos aquí el bucle WHILE
-        # --> Implemente el código del Bucle
-    elif n==1 :
-        vector[0]=1
+        contador = 2
+        while contador < n:
+            siguiente = vector[contador - 1] + vector[contador - 2]
+            vector.append(siguiente)
+            contador += 1
 
-    return vector; # Retorno de la función
-
-# Programa principal
-def main():
-    print("SERIE DE FIBONACCI")
-    numero=int(input("Introduzca un numero: "))
-    print("{0} elementos --> FIBONACCI: {1}.".format(numero,fibonacci(numero)))
-
-if __name__== "__main__" :
-   main()
+    return vector  # Retorno
